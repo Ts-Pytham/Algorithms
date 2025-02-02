78a5ab612683f93fa1616b3c9c1ef7d79a3fd4c7
Ts-Pytham <johansanchezal@unimagdalena.edu.co>
2025-02-02 02:04:42 -0500

    first commit

---
+++ Algorithms/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Algorithms.Math;
+using System.Collections.Generic;
+
+namespace Algorithms;
+
+public static class Class1
+{
+    public static void Main()
+    {
+        int a = 10;
+        int b = 20;
+        int gcd = a.GCD(b);
+        int lcm = ArithmeticAlgorithm.LCM(a, b);
+        ArithmeticAlgorithm.GCD(a, b);
+    }
+
+
+}
