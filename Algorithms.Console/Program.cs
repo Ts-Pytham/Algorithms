8ca94cc056fe6f858154286440e0b40f190eca2c
Ts-Pytham <johansanchezal@unimagdalena.edu.co>
2025-02-02 16:44:42 -0500

    Añadir proyecto de consola y método IsPrime
    Se ha añadido un nuevo proyecto de consola `Algorithms.Console` al archivo de solución `Algorithms.sln`.
    Se ha eliminado el archivo `Class1.cs` con ejemplos de `GCD` y `LCM`.
    Se ha creado el archivo de proyecto `Algorithms.Console.csproj` para .NET 9.0.
    Se ha añadido `Program.cs` en el proyecto de consola para verificar números primos.
    Se ha añadido `ArithmeticAlgorithm.primes.cs` con la implementación del método `IsPrime` usando el algoritmo de división de prueba.

---
+++ Algorithms.Console/Program.cs
@@ -0,0 +1,4 @@
+﻿using Algorithms.Math.Arithmetic;
+
+long a = 12;
+Console.WriteLine($"a es primo: {a.IsPrime()}");
---
+++ Algorithms/Math/Arithmetic/ArithmeticAlgorithm.primes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Algorithms.Math.Arithmetic;
+
+public static partial class ArithmeticAlgorithm
+{
+    /// <summary>
+    /// Determines whether a given number is prime.
+    /// </summary>
+    /// <remarks>
+    /// The method uses the trial division algorithm to determine whether a given number is prime.
+    /// The algorithm divides the number by all integers from 2 to the square root of the number.
+    /// If the number is divisible by any of these integers, it is not prime.
+    /// </remarks>
+    /// <param name="number">The number to be checked for primality.</param>
+    /// <returns>
+    /// Returns <c>true</c> if the number is prime; otherwise, <c>false</c>.
+    /// </returns>
+    public static bool IsPrime(this long number)
+    { 
+        if (number < 2 || number % 2 == 0)
+        {
+            return false;
+        }
+        else if (number < 4)
+        {
+            return true;
+        }
+        var limit = System.Math.Sqrt(number);
+
+        for (int i = 3; i <= limit; i += 2)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
